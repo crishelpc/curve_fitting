7bed941d74b304b98ef4ab3405fa915ff4d41fbc
23jammy <23jammycruz@gmail.com>
2024-12-10 10:11:15 +0800

    Test for plot

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,13 @@
-y_values
+tter(x, y)
+plt.plot(x, y)
+
+plt.scatter(x, norm1)
+plt.plot(x, norm1)
+
+plt.scatter(x, norm2)
+plt.plot(x, norm2)
+
+plt.scatter(x, norm3)
+plt.plot(x, norm3)
+
+plt.show()
--- test.py
+++ test.py
@@ -1,10 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline 
 
-x = [0,5,8,12,16,20,23,31]
-y = [3,3,5,6,15,17,11,18]
+x = np.array([0,5,8,12,16,20,23,31])
+y = np.array([3,3,5,6,15,17,11,18])
+norm1 = np.array([0.97656735, 3.72228227, 5.36971122, 7.56628315, 9.76285509, 11.95942702, 13.60685598, 17.99999985])
+norm2 = np.array([1.8928875, 4.62579613, 6.2655413, 8.45186821, 10.63819511, 12.82452201, 14.46426719, 18.83692099])
+norm3 = np.array([4.4433098, 6.64852549, 7.9716549, 9.73582745, 11.5, 13.26417255, 14.58730196, 18.11564706])
 
-plt.scatter(x, y)
-plt.plot(x, y)
+# plt.scatter(x, y)
+# plt.plot(x, y)
+
+# plt.scatter(x, norm1)
+# plt.plot(x, norm1)
+
+# plt.scatter(x, norm2)
+# plt.plot(x, norm2)
+
+# plt.scatter(x, norm3)
+# plt.plot(x, norm3)
+
+# plt.show()
+
+xnew = np.linspace(x.min(), x.max(), 100) 
+gfg1 = make_interp_spline(x, y, k=2) 
+ynew = gfg1(xnew) 
+
+gfg2 = make_interp_spline(x, norm1, k=3) 
+n1new = gfg2(xnew) 
+
+gfg3 = make_interp_spline(x, norm2, k=3) 
+n2new = gfg3(xnew) 
+
+gfg4 = make_interp_spline(x, norm3, k=3) 
+n3new = gfg4(xnew) 
+
+plt.plot(xnew, ynew)
+
+plt.plot(xnew, n1new)
+
+plt.plot(xnew, n2new)
+
+plt.plot(xnew, n3new)
 
 plt.show()
